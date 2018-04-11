1549bd3af6313bd4c841757c570f5db40f502e50
Piergiorgio Faraglia <p.faraglia@gmail.com>
2018-04-11 14:20:56 +0200

    Adds log

--- dbdump_folder_cleaner.sh
+++ dbdump_folder_cleaner.sh
@@ -7,6 +7,12 @@
 #
 
 ###############################################################################
+# Dependencies
+###############################################################################
+
+# bc
+
+###############################################################################
 # Variables
 ###############################################################################
 
@@ -22,13 +28,17 @@ KEEP_ALIVE_DUMPS=10;
 NUM_DUMPS=`ls -1 "${BASEDIR}" | wc -l`
 DUMP_TO_DELETE=$(echo "${NUM_DUMPS}-${KEEP_ALIVE_DUMPS}"|bc)
 
+echo "`date +%Y%m%d%H%M%S` - BEGIN execution.";
+
 if [ $DUMP_TO_DELETE -eq 0 ]; then
-    echo "No exceeded dump found. Nothing to delete."
+    echo "`date +%Y%m%d%H%M%S` - No exceeded dump found. Nothing to delete."
     exit
 fi
 
 for i in `ls -1 ${BASEDIR} | head -${DUMP_TO_DELETE}`; do
-    echo removing item: "${BASEDIR}$i"
+    echo "`date +%Y%m%d%H%M%S` - removing item: ${BASEDIR}$i"
     rm -v "${BASEDIR}$i"
 done
 
+echo "`date +%Y%m%d%H%M%S` - END execution.";
+echo "";
--- dbdump_mysql_cron.sh
+++ dbdump_mysql_cron.sh
@@ -35,11 +35,18 @@ zip_command_path='/bin/gzip';
 # Get the current datatime
 CURRENT_DATETIME=`date +%Y%m%d%H%M%S`;
 
+echo "`date +%Y%m%d%H%M%S` - BEGIN execution.";
+
 dump_name="${CURRENT_DATETIME}.${db_name}.dump.sql";
 dump_path="${dumps_dir}${dump_name}";
 
 # Database's dump
 "${dump_command_path}" --single-transaction -u "${db_user}" -p${db_pass} "${db_name}" > "${dump_path}";
+echo "`date +%Y%m%d%H%M%S` - Dumped db with command: ${dump_command_path} --single-transaction -u ${db_user} ${db_name} > ${dump_path}";
 
 # Dump compression using gzip
 ${zip_command_path} "${dump_path}";
+echo "`date +%Y%m%d%H%M%S` - Compressing dump with command: ${zip_command_path} ${dump_path}";
+
+echo "`date +%Y%m%d%H%M%S` - END execution.";
+echo ""
