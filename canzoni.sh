cef61f961dfef427a5b21160fcec2398cd10e458
Piergiorgio Faraglia <p.faraglia@gmail.com>
2020-09-25 14:42:32 +0200

    Aggiungo script

---
+++ canzoni.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=DJ-3-zMT4fE
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=VgfBPDxFdm4&list=PLjUPTquQ5wr4v2nvQHHp_bETjCeLK9s6-
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=HXhymeebw0U
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=DmJhNkkvX4s
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=Afe9_yif6h4
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/fNUAqFRIMaE
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=XaKPbXXbtWY
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=o-mybJKgcVk
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=iXFAunwnIxE
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=rJWZhitXWzI
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/27JleM39TPY
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/OkvXugiij3U
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/WHIM2LCmVVc
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/ypcWAQ4Wl4U
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/rBpBBuRqxfc
+youtube-dl --extract-audio --audio-format mp3 https://youtu.be/J9mTFhfHbck
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=PR82zuSMugQ
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=EXTUGbI7Wvc&t=35s
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=brh5nM3oksU
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=CjHyMQ32HhI
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=6ehoNmD5SSE
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=lPN1DlS1-Pk
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=5XiZRt_Pxqw
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=WAnBw4w8FB0
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=xeJUMRVAKVg
---
+++ kita.sh
@@ -0,0 +1,2 @@
+# youtube-dl -o '%(title)s.%(ext)s' 
+youtube-dl -o '%(title)s.%(ext)s' https://www.youtube.com/watch?v=ymigWt5TOV8
